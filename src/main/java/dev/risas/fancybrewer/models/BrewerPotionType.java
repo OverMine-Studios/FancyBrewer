5a43faa36918cc2c6ed0b7ea7cba2ebbc00481ad
Mauricio <mauriciouch15@gmail.com>
2024-02-08 20:25:17 -0300

    feat ✅: added more potions types

---
+++ src/main/java/dev/risas/fancybrewer/models/BrewerPotionType.java
@@ -0,0 +1,144 @@
+package dev.risas.fancybrewer.models;
+
+import com.cryptomorin.xseries.XMaterial;
+import dev.risas.fancybrewer.utilities.item.ItemBuilder;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+public enum BrewerPotionType {
+
+    INSTANT_HEALTH(
+            "Potion of Healing",
+            ItemBuilder.createPotion("INSTANT_HEAL", 8229, false,true, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), XMaterial.GLISTERING_MELON_SLICE.parseMaterial(), Material.GLOWSTONE_DUST),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    INSTANT_HEALTH_SPLASH(
+            "Potion of Healing",
+            ItemBuilder.createPotion("INSTANT_HEAL", 16421, false, true, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), XMaterial.GLISTERING_MELON_SLICE.parseMaterial(), Material.GLOWSTONE_DUST, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    SPEED_I(
+            "Potion of Swiftness",
+            ItemBuilder.createPotion("SPEED", 8194, false, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1)),
+    SPEED_I_EXTENDED(
+            "Potion of Swiftness",
+            ItemBuilder.createPotion("SPEED", 8258, true, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR, Material.REDSTONE),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    SPEED_II(
+            "Potion of Swiftness",
+            ItemBuilder.createPotion("SPEED", 8226, false, true, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR, Material.GLOWSTONE_DUST),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    SPEED_I_SPLASH(
+            "Potion of Swiftness",
+            ItemBuilder.createPotion("SPEED", 16386, false, false, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    SPEED_I_SPLASH_EXTENDED(
+            "Potion of Swiftness",
+            ItemBuilder.createPotion("SPEED", 16450, true, false, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR, Material.REDSTONE, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    SPEED_II_SPLASH(
+            "Potion of Swiftness",
+            ItemBuilder.createPotion("SPEED", 16418, false, true, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR, Material.GLOWSTONE_DUST, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    STRENGTH_I(
+            "Potion of Strength",
+            ItemBuilder.createPotion("STRENGTH", 8201, false, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.BLAZE_POWDER),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1)),
+    STRENGTH_I_EXTENDED(
+            "Potion of Strength",
+            ItemBuilder.createPotion("STRENGTH", 8265, true, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.BLAZE_POWDER, Material.REDSTONE),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    STRENGTH_II(
+            "Potion of Strength",
+            ItemBuilder.createPotion("STRENGTH", 8233, false, true, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.BLAZE_POWDER, Material.GLOWSTONE_DUST),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    STRENGTH_I_SPLASH(
+            "Potion of Strength",
+            ItemBuilder.createPotion("STRENGTH", 16393, false, false, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.BLAZE_POWDER, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    STRENGTH_I_SPLASH_EXTENDED(
+            "Potion of Strength",
+            ItemBuilder.createPotion("STRENGTH", 16457, true, false, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.BLAZE_POWDER, Material.REDSTONE, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    STRENGTH_II_SPLASH(
+            "Potion of Strength",
+            ItemBuilder.createPotion("STRENGTH", 16425, false, true, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.BLAZE_POWDER, Material.GLOWSTONE_DUST, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    INVISIBILITY(
+            "Potion of Invisibility",
+            ItemBuilder.createPotion("INVISIBILITY", 8238, false, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.GOLDEN_CARROT, Material.FERMENTED_SPIDER_EYE),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    INVISIBILITY_EXTENDED(
+            "Potion of Invisibility",
+            ItemBuilder.createPotion("INVISIBILITY", 8270, true, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.GOLDEN_CARROT, Material.FERMENTED_SPIDER_EYE, Material.REDSTONE),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    FIRE_RESISTANCE(
+            "Potion of Fire Resistance",
+            ItemBuilder.createPotion("FIRE_RESISTANCE", 8259, true, false, false),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.MAGMA_CREAM, Material.REDSTONE),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2)),
+    POISON_SPLASH(
+            "Potion of Poison",
+            ItemBuilder.createPotion("POISON", 16388, true, false, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SPIDER_EYE, Material.REDSTONE, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3)),
+    SLOWNESS_SPLASH(
+            "Potion of Slowness",
+            ItemBuilder.createPotion("SLOWNESS", 16394, false, false, true),
+            Arrays.asList(XMaterial.NETHER_WART.parseMaterial(), Material.SUGAR, Material.FERMENTED_SPIDER_EYE, XMaterial.GUNPOWDER.parseMaterial()),
+            Arrays.asList(BrewerPotionStage.AWKWARD, BrewerPotionStage.INGREDIENT_1, BrewerPotionStage.INGREDIENT_2, BrewerPotionStage.INGREDIENT_3));
+
+
+    private final String name;
+    private final List<Material> ingredients;
+    private final List<BrewerPotionStage> stages;
+    private final ItemStack result;
+
+    BrewerPotionType(String name, ItemStack result, List<Material> ingredients, List<BrewerPotionStage> stages) {
+        this.name = name;
+        this.result = result;
+        this.ingredients = ingredients;
+        this.stages = stages;
+    }
+
+    public String getPlaceholder(int slot) {
+        Material ingredientSlot = ingredients.get(slot);
+
+        if (ingredientSlot != null) {
+            if (ingredientSlot.equals(Material.GLOWSTONE_DUST)) {
+                return "II";
+            }
+            else if (ingredientSlot.equals(Material.REDSTONE)) {
+                return "Extended";
+            }
+            else if (ingredientSlot.equals(XMaterial.GUNPOWDER.parseMaterial())) {
+                return "Splash";
+            }
+            else if (ingredientSlot.equals(Material.FERMENTED_SPIDER_EYE)) {
+                return "Invisibility";
+            }
+        }
+        return "";
+    }
+}
+
---
+++ src/main/java/dev/risas/fancybrewer/models/menu/buttons/BrewingStartButton.java
@@ -0,0 +1,117 @@
+package dev.risas.fancybrewer.models.menu.buttons;
+
+import dev.risas.fancybrewer.FancyBrewerPlugin;
+import dev.risas.fancybrewer.models.Brewer;
+import dev.risas.fancybrewer.models.BrewerPotionStage;
+import dev.risas.fancybrewer.models.BrewerState;
+import dev.risas.fancybrewer.resources.types.ConfigResource;
+import dev.risas.fancybrewer.resources.types.LanguageResource;
+import dev.risas.fancybrewer.utilities.ChatUtil;
+import dev.risas.fancybrewer.utilities.cooldown.CooldownUtil;
+import dev.risas.fancybrewer.utilities.item.ItemBuilder;
+import dev.risas.fancybrewer.utilities.menu.Button;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BrewingStartButton extends Button {
+
+    private final FancyBrewerPlugin plugin;
+    private final Brewer brewer;
+
+    public BrewingStartButton(FancyBrewerPlugin plugin, Brewer brewer) {
+        this.plugin = plugin;
+        this.brewer = brewer;
+    }
+
+    @Override
+    public ItemStack getButtonItem(Player player) {
+        if (brewer.getState() == BrewerState.BREWING) {
+            ItemStack itemStack = ConfigResource.BREWING_MENU_BUTTONS_START_BREWING.clone();
+            BrewerPotionStage stage = brewer.getCurrentStage();
+            List<String> lore = new ArrayList<>();
+
+            for (String line : itemStack.getItemMeta().getLore()) {
+                if (line.contains("<stage-potion-type-3>") && brewer.getPotionType().getStages().contains(BrewerPotionStage.INGREDIENT_2)) {
+                    lore.add(line
+                            .replace("<stage-potion-type-3>", ConfigResource.BREWING_MENU_BUTTONS_START_BREWING_STAGE_POTION_TYPES.get(2)
+                                    .replace("<stage-potion-placeholder>", brewer.getPotionType().getPlaceholder(2))
+                                    .replace("<stage-potion-type>", brewer.getPotionType().getName())
+                                    .replace("<stage-color>", stage.getColorStage(BrewerPotionStage.INGREDIENT_2))));
+                    continue;
+                }
+                if (line.contains("<stage-potion-type-4>") && brewer.getPotionType().getStages().contains(BrewerPotionStage.INGREDIENT_3)) {
+                    lore.add(line
+                            .replace("<stage-potion-type-4>", ConfigResource.BREWING_MENU_BUTTONS_START_BREWING_STAGE_POTION_TYPES.get(3)
+                                    .replace("<stage-potion-placeholder>", brewer.getPotionType().getPlaceholder(3))
+                                    .replace("<stage-potion-type>", brewer.getPotionType().getName())
+                                    .replace("<stage-color>", stage.getColorStage(BrewerPotionStage.INGREDIENT_3))));
+                    continue;
+                }
+
+                lore.add(line
+                        .replace("<stage-potion-type-1>", ConfigResource.BREWING_MENU_BUTTONS_START_BREWING_STAGE_POTION_TYPES.get(0)
+                                .replace("<stage-color>", stage.getColorStage(BrewerPotionStage.AWKWARD)))
+                        .replace("<stage-potion-type-2>", ConfigResource.BREWING_MENU_BUTTONS_START_BREWING_STAGE_POTION_TYPES.get(1)
+                                .replace("<stage-color>", stage.getColorStage(BrewerPotionStage.INGREDIENT_1)))
+                        .replace("<stage-potion-type>", brewer.getPotionType().getName())
+                        .replace("<stage-phase>", String.valueOf(brewer.getCurrentStage().getIndex() + 1))
+                        .replace("<stage-phase-time>", brewer.getCurrentStage().getStageCooldownFormatted())
+                        .replace("<time>", brewer.getEstimatedTimeRemainingFormatted()));
+            }
+
+            lore.removeIf(line -> line.contains("<stage-potion-type-3>") && !brewer.getPotionType().getStages().contains(BrewerPotionStage.INGREDIENT_2));
+            lore.removeIf(line -> line.contains("<stage-potion-type-4>") && !brewer.getPotionType().getStages().contains(BrewerPotionStage.INGREDIENT_3));
+
+            return new ItemBuilder(itemStack)
+                    .setLore(lore)
+                    .build();
+        }
+        ItemStack itemStack = ConfigResource.BREWING_MENU_BUTTONS_START_NOT_BREWING.clone();
+        return new ItemBuilder(itemStack)
+                .setLore(itemStack.getItemMeta().getLore()
+                        .stream().map(line -> line
+                                .replace("<time>", brewer.getEstimatedTimeRemainingFormatted()))
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    @Override
+    public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+        if (CooldownUtil.hasCooldown(player, "brewing-start-button")) {
+            ChatUtil.sendMessage(player, "&cYou can't click this button so fast!");
+            return;
+        }
+
+        CooldownUtil.setCooldown(plugin, player, "brewing-start-button", 1);
+
+        if (brewer.getState() == BrewerState.IDLE) {
+            if (brewer.getCurrentStage() == null) {
+                if (!brewer.hasMinBottles()) {
+                    playFail(player);
+                    ChatUtil.sendMessage(player, LanguageResource.BREWER_MESSAGES_NEED_BOTTLES);
+                    return;
+                }
+
+                if (brewer.getPotionType() == null) {
+                    playFail(player);
+                    ChatUtil.sendMessage(player, LanguageResource.BREWER_MESSAGES_NEED_INGREDIENTS);
+                    return;
+                }
+            }
+
+            playSuccess(player);
+            brewer.setState(BrewerState.BREWING);
+            brewer.startBrewing(plugin);
+            brewer.open(player, plugin);
+        }
+        else if (brewer.getState() == BrewerState.BREWING) {
+            playSuccess(player);
+            brewer.setState(BrewerState.IDLE);
+        }
+    }
+}
